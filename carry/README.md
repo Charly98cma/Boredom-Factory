1947beaddea23d6f53f6f479ed3cdfd9e513a600
Charly98cma <1998cma@gmail.com>
2022-07-10 15:52:32 +0200

    carry: First commit

---
+++ carry/README.md
@@ -0,0 +1,5 @@
+# carry - Command line Cut-&-Paste
+
+This project is a sort of *Cut-&-Paste* command that allows moving multiple
+files and directories from one place to another (just like a graphical file
+manager).
---
+++ carry/carry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PACKAGE="carry"
+DIR="/tmp/.carry"
+
+function help {
+    echo ""
+}
+
+function main {
+    return
+}
+
+function carry {
+    return
+}
+
+function drop {
+    return
+}
+
+main
+exit 0
